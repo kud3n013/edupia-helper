31d62e4a8fba71425a27a5e9a6af76aba2bb57c8
kud3n.013 <kud3n.013@proton.me>
2026-01-09 15:00:31 +0700

    Deployment fix

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -7,7 +7,7 @@ export function SmoothScrolling() {
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             direction: "vertical", // vertical, horizontal
             gestureDirection: "vertical", // vertical, horizontal, both
             smooth: true,
